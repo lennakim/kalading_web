3de0a552f840ef716e417cb487fba1722d24dd64
teng.cong <congteng45@gmail.com>
2014-12-02 14:54:10 +0800

    add api wrapper

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
 //= require underscore
 //= require backbone
 //= require backbone_rails_sync
 //= require backbone_datalink
 //= require backbone/kalading_web
-//= require_tree .
