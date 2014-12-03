72563c6b93fc9f49145a8f61f40bd740890e2bf5
teng.cong <congteng45@gmail.com>
2014-12-03 11:38:16 +0800

    SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+  def title(title = nil)
+    if title.present?
+      content_for :title, title
+    else
+      content_for?(:title) ? Settings.default_title + ' | ' + content_for(:title) : Settings.default_title
+    end
+  end
+
+  def meta_keywords(tags = nil)
+    if tags.present?
+      content_for :meta_keywords, tags
+    else
+      content_for?(:meta_keywords) ? [content_for(:meta_keywords), Settings.meta_keywords].join(', ') : Settings.meta_keywords
+    end
+  end
+
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : Settings.meta_description
+    end
+  end
+
 end
