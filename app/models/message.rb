23298491076cc4d0b272d32179736161423f5d2d
ohcoder <coderliang@gmail.com>
2014-12-10 11:53:29 +0800

    add begin/rescue to handle mysql errors

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,32 +1,40 @@
 class Message < ActiveRecord::Base
-  validates :msg_id, presence: true
+  validates :msg_id, presence: true, :uniqueness => :true
   belongs_to :user
 
   def self.save_txt_message(account_name, message)
-    user = User.find_by(openid: message.FromUserName)
-    user = User.create_weixin_user account_name, message.FromUserName unless user
+      user = User.find_by(openid: message.FromUserName)
+      user = User.create_weixin_user account_name, message.FromUserName unless user
 
-    new_message = user.messages.new
-    new_message.to_user_name   = message.ToUserName
-    new_message.from_user_name = message.FromUserName
-    new_message.msg_type       = message.MsgType
-    new_message.content        = message.Content
-    new_message.msg_id         = message.MsgId
-    new_message.save!
+    begin
+      new_message = user.messages.new
+      new_message.to_user_name   = message.ToUserName
+      new_message.from_user_name = message.FromUserName
+      new_message.msg_type       = message.MsgType
+      new_message.content        = message.Content
+      new_message.msg_id         = message.MsgId
+      new_message.save!
+    rescue ActiveRecord::RecordInvalid => e
+      Rails.logger.error e
+    end
   end
 
   def self.save_img_message(account_name, message)
-    user = User.find_by(openid: message.FromUserName)
-    user = User.create_weixin_user account_name, message.FromUserName unless user
+      user = User.find_by(openid: message.FromUserName)
+      user = User.create_weixin_user account_name, message.FromUserName unless user
 
-    new_message = user.messages.new
-    new_message.to_user_name   = message.ToUserName
-    new_message.from_user_name = message.FromUserName
-    new_message.msg_type       = message.MsgType
-    new_message.pic_url        = message.PicUrl
-    new_message.media_id       = message.MediaId
-    new_message.msg_id         = message.MsgId
-    new_message.save!
+    begin
+      new_message = user.messages.new
+      new_message.to_user_name   = message.ToUserName
+      new_message.from_user_name = message.FromUserName
+      new_message.msg_type       = message.MsgType
+      new_message.pic_url        = message.PicUrl
+      new_message.media_id       = message.MediaId
+      new_message.msg_id         = message.MsgId
+      new_message.save!
+    rescue ActiveRecord::RecordInvalid => e
+      Rails.logger.error e
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  validates :openid, :uniqueness => true
   belongs_to :public_account
   has_many :messages
 
@@ -13,21 +14,25 @@ class User < ActiveRecord::Base
   end
 
   def self.create_weixin_user(account_name, openid)
-    user = User.new
-    account ||= PublicAccount.get_account(account_name)
-    user_info = account.user(openid)
-    user.subscribe      = user_info.result["subscribe"]
-    user.openid         = user_info.result["openid"]
-    user.nickname       = user_info.result["nickname"]
-    user.sex            = user_info.result["sex"]
-    user.language       = user_info.result["language"]
-    user.city           = user_info.result["city"]
-    user.province       = user_info.result["province"]
-    user.country        = user_info.result["country"]
-    user.headimgurl     = user_info.result["headimgurl"]
-    user.subscribe_time = user_info.result["subscribe_time"]
-    user.save!
-    user
+    begin
+      user = User.new
+      account ||= PublicAccount.get_account(account_name)
+      user_info = account.user(openid)
+      user.subscribe      = user_info.result["subscribe"]
+      user.openid         = user_info.result["openid"]
+      user.nickname       = user_info.result["nickname"]
+      user.sex            = user_info.result["sex"]
+      user.language       = user_info.result["language"]
+      user.city           = user_info.result["city"]
+      user.province       = user_info.result["province"]
+      user.country        = user_info.result["country"]
+      user.headimgurl     = user_info.result["headimgurl"]
+      user.subscribe_time = user_info.result["subscribe_time"]
+      user.save!
+      user
+    rescue ActiveRecord::RecordInvalid => e
+      Rails.logger.error e
+    end
   end
 
   def set_weixin_user_info(account_name, openid)
