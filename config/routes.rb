6bd0ffbfef77e2c7196cfdeb26637840b8ba66af
teng.cong <congteng45@gmail.com>
2014-12-11 12:46:43 +0800

    remove useless comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   mount WeixinRailsMiddleware::Engine, at: "/"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
   resources :orders do
     get 'select_car',     on: :collection
     get 'select_item',    on: :collection
+
     post 'refresh_price', on: :collection
     post 'place_order',   on: :collection
     post 'submit_order',  on: :collection
