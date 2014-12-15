c8aec061537ff423f75f58689841ac3a75ef52ed
teng.cong <congteng45@gmail.com>
2014-12-15 12:50:17 +0800

    fix yunpian api wrapper

--- lib/yunpian_api.rb
+++ lib/yunpian_api.rb
@@ -4,14 +4,12 @@ module YunpianApi
   extend self
 
   def send_to mobile, content
-    data = {
+    payload = {
       apikey: 'b898453f2ea218bbbe953ae0208d11dc',
       mobile: mobile,
       text: content
     }
 
-    RestClient.post \
-      "http://yunpian.com/v1/sms/tpl_send.json",
-      data.to_json
+    RestClient.post "http://yunpian.com/v1/sms/send.json", payload
   end
 end
