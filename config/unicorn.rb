0dd2f2fe9243df254e64fd0670e565e0df3ac9a8
teng.cong <congteng45@gmail.com>
2014-12-24 22:55:17 +0800

    unicorn & nginx configurations

---
+++ config/unicorn.rb
@@ -0,0 +1,11 @@
+root = "/home/deployer/projects/kalading_web"
+
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.blog.sock"
+worker_processes 2
+timeout 30
+
