e60fdae74e64b5c8e3e4896bc5a72ec0a0ed2c70
teng.cong <congteng45@gmail.com>
2014-12-24 23:08:36 +0800

    fix unicorn conf bug

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.kalading.sock"
 worker_processes 2
 timeout 30
 
