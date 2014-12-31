8fc58485208842a019fa462d664362af77501ea6
teng.cong <congteng45@gmail.com>
2014-12-31 14:21:58 +0800

    get current city

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user, :signed_in?
+  helper_method :current_city
 
   before_action :set_city
 
@@ -20,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user_locale
-    return current_user.city if current_user && current_user.city
+    return current_user.city.name if current_user && current_user.city
 
     cookies[:city]
   end
