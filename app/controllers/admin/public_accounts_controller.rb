1d4bc03b02876006fa1ecfe2d6c82133ce6b6564
ohcoder <coderliang@gmail.com>
2015-01-04 12:15:06 +0800

    fixed a _form submit problem

--- app/controllers/admin/public_accounts_controller.rb
+++ app/controllers/admin/public_accounts_controller.rb
@@ -1,5 +1,23 @@
 class Admin::PublicAccountsController < Admin::MainController
-  def new
+
+  before_action :set_account, only: [:new, :edit, :update, :destroy, :apply]
+
+  def create
+    # render plain: params.inspect
+  end
+
+  def update
+    render plain: menu_params
+  end
+
+  private
+
+  def set_account
     @account = PublicAccount.find_by(name: "kalading1")
   end
+
+  def menu_params
+    params.require(:public_account).permit(diymenus_attributes: [:id, :key, :name, :url, :is_show, :_destroy, sub_menus_attributes: [:id, :key, :name, :url, :is_show, :_destroy]])
+  end
+
 end
