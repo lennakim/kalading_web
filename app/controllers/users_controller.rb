560f7ec8a197fbec2121f6000d3cdb2633a9a54d
teng.cong <congteng45@gmail.com>
2015-01-05 23:47:23 +0800

    user orders page basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,22 @@ class UsersController < ApplicationController
 
   def orders
     # phone_nums = params[:phone_nums]
-    @orders = Order.get_orders_of '13333333333'
+    # @orders = Order.get_orders_of '13333333333'
+    @orders = Order.get_orders_of '13111111111'
   end
 
   def maintain_histories
-  	@maintain_orders = Order.maintain_histories_of '13111111111'
+    @maintain_orders = Order.maintain_histories_of '13111111111'
     pp @maintain_orders
   end
 
   def cars
-    
+
   end
-  
+
   def new
     @cars_info = Order.cars_data
-    pp @cars_info
-  end 
+  end
 
   def balance
   end
