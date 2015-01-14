5910bea87d7a75a555fad5419209c706f5f8f9a2
teng.cong <congteng45@gmail.com>
2015-01-14 21:43:14 +0800

    login before checkout orders and maintain histories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,19 @@ class UsersController < ApplicationController
   end
 
   def maintain_histories_list
+
+    redirect_to root_path(login: 1) unless signed_in?
+
     @maintain_orders = Order.maintain_histories_of current_user.phone_number
   end
 
   def maintain_history
+    redirect_to root_path(login: 1) unless signed_in?
     @maintain_history = Order.api_maintain_find params[:id]
   end
 
   def settings
+    redirect_to root_path(login: 1) unless signed_in?
     @cities = Order.cities
   end
 
@@ -26,6 +31,7 @@ class UsersController < ApplicationController
   end
 
   def cars
+    redirect_to root_path(login: 1) unless signed_in?
     @cars_info = Order.cars_data current_city_id
     @auto_id = params[:auto_id]
     @maintain_histories = Order.maintain_report login_phone_num: current_user.phone_number, car_id: params[:car_id], car_num: params[:car_num]
