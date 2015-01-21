2684354bd40ef85d0daad9252e245576e31beb84
teng.cong <congteng45@gmail.com>
2015-01-21 13:43:12 +0800

    配置staging服务器访问http basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
   before_action :set_city
   before_action :set_device_type
 
+  http_basic_authenticate_with name: 'staging', password: 'abcdefg' if Rails.env == "staging"
+
   def sign_in user
     cookies[:LGT] = { value: user.update_user, expires: 30.days.from_now }
   end
