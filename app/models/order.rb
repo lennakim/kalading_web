a2ab9b46707f4aca8b0a1f09c919661dd75da3c2
teng.cong <congteng45@gmail.com>
2015-01-22 13:03:29 +0800

    修复语法错误

--- app/models/order.rb
+++ app/models/order.rb
@@ -50,7 +50,7 @@ class Order
     end
 
     def items_for car_id, city_id, type = "bmt"
-      ServerApi.call "get", "auto_maintain_order", { entry_id: car_id, city_id: city_id, "#{type}": true } {[]}
+      ServerApi.call "get", "auto_maintain_order", { entry_id: car_id, city_id: city_id, "#{type}" => true } {[]}
     end
 
     def refresh_price car_id, city_id, payload
