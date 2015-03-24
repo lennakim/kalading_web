aa9cc791b417d6eb9a53ea73959e1373c3df0340
teng.cong <congteng45@gmail.com>
2015-03-24 17:57:26 +0800

    后台管理用户权限

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -1,2 +1,12 @@
 module Admin::AdminHelper
+  def can? action
+    return false if !current_user
+
+    if action == :manage
+      current_user.role == "administrator"
+    elsif action == :edit
+      current_user.role == "editor" ||
+      current_user.role == "administrator"
+    end
+  end
 end
