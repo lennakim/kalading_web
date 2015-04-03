5b17fadecbdad7a1196785c7b6e698e30bcb708b
OhCoder <coderliang@gmail.com>
2015-04-03 09:34:26 +0800

    修改关联account问题

--- app/controllers/admin/reply_messages_controller.rb
+++ app/controllers/admin/reply_messages_controller.rb
@@ -4,8 +4,9 @@ class Admin::ReplyMessagesController < Admin::MainController
   actions :all, :except => [ :show ]
 
   def create
+    @reply_message = ReplyMessage.new permitted_params[:reply_message]
     account = PublicAccount.find_by(name:"kaladingcom")
-    account.reply_messages.new permitted_params[:reply_message]
+    account.reply_messages << @reply_message
     create!
   end
 
