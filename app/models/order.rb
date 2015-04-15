78557aa5491f1e6f3e8e69aea097ebc37e0f5241
j <rabit.jia@gmail.com>
2015-04-15 14:40:37 +0800

    订单评价列表

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,7 +11,7 @@ class Order
     end
 
     def comments
-      ServerApi.call "get", "order_evaluation_list"
+      ServerApi.call "get", "/api/v2/evaluations"
     end
 
     def auto_brands city_id
