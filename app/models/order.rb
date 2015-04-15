4bb917d2b1bcb2566887936502586cd09229baec
teng.cong <congteng45@gmail.com>
2015-04-15 18:12:00 +0800

    临时去掉所有评论

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,7 +11,8 @@ class Order
     end
 
     def comments
-      ServerApi.call "get", "/api/v2/evaluations"
+      # ServerApi.call "get", "/api/v2/evaluations"
+      []
     end
 
     def auto_brands city_id
