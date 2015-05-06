253428e26acd8fb18fe17e28acb763396cda568f
shooter <shooter.hero@gmail.com>
2015-05-07 01:06:13 +0800

    取消 http basic auth

--- app/controllers/doc_controller.rb
+++ app/controllers/doc_controller.rb
@@ -1,10 +1,6 @@
 class DocController < ApplicationController
   layout "api"
 
-  if Rails.env.production? || Rails.env.staging?
-    http_basic_authenticate_with name: "kalading", password: "secret"
-  end
-
   def v2
   end
 end
