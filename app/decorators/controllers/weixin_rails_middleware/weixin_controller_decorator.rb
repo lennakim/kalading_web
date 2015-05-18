4a0e99c9ac8dbad5f52cc9815b2e799902e04243
OhCoder <coderliang@gmail.com>
2015-05-18 15:12:46 +0800

    测试

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -15,7 +15,7 @@ WeixinRailsMiddleware::WeixinController.class_eval do
       message = account.recv_messages.new
       message.set_info msg
       message.save
-      Rails.logger.info("msg is #{message.content}, the save state is #{message.save}")
+      Rails.logger.info("msg is #{message.content}, the save state is #{message.save}. RecvMessages.all are #{RecvMessages.all}")
     end
 
     def auto_response(keyword, account_id)
